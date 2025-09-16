ae8104e03cf32e660794d85b64b7431a7b3192ef
„kriztovv“ <kriztovv@gmail.com  git config --global user.name kriztovv  git config --global user.email kriztovv@gmail.com>
2025-09-16 09:56:31 +0200

    multiple boxes!

--- main.js
+++ main.js
@@ -5,13 +5,17 @@ let startX = 0,
 
 const card = document.getElementById("card");
 gsap.to(card, {
-      transform: "scale(1)",
-            duration: "0.2"
+  transform: "scale(1)",
+  duration: "0.2",
 });
 
-const dropper = document.getElementById("drop");
+const dropper1 = document.getElementById("drop1");
+const dropper2 = document.getElementById("drop2");
+const dropper3 = document.getElementById("drop3");
 
-const domRect2 = dropper.getBoundingClientRect();
+const domRect2 = dropper1.getBoundingClientRect();
+const domRect3 = dropper2.getBoundingClientRect();
+const domRect4 = dropper3.getBoundingClientRect();
 
 let isLocked = 0;
 
@@ -35,8 +39,7 @@ function mouseMove(e) {
   card.style.top = card.offsetTop - newY + "px";
   card.style.left = card.offsetLeft - newX + "px";
 
-  console.log();
-
+  //box1
   const domRect1 = card.getBoundingClientRect();
 
   if (
@@ -48,42 +51,103 @@ function mouseMove(e) {
     )
   ) {
     isLocked = 1;
+    container = 1;
+    console.log(container);
     gsap.to(card, {
       transform: "scale(1.2)",
-      duration: "0.2"
+      duration: "0.2",
     });
   }
 
-  if (
-    domRect1.top > domRect2.bottom ||
-    domRect1.right < domRect2.left ||
-    domRect1.bottom < domRect2.top ||
-    domRect1.left > domRect2.right
+  //box2
+  else if (
+    !(
+      domRect1.top > domRect3.bottom ||
+      domRect1.right < domRect3.left ||
+      domRect1.bottom < domRect3.top ||
+      domRect1.left > domRect3.right
+    )
   ) {
+    isLocked = 1;
+    container = 2;
+    console.log(container);
+    gsap.to(card, {
+      transform: "scale(1.2)",
+      duration: "0.2",
+    });
+  }
+
+  //box3
+  else if (
+    !(
+      domRect1.top > domRect4.bottom ||
+      domRect1.right < domRect4.left ||
+      domRect1.bottom < domRect4.top ||
+      domRect1.left > domRect4.right
+    )
+  ) {
+    isLocked = 1;
+    container = 3;
+    console.log(container);
+    gsap.to(card, {
+      transform: "scale(1.2)",
+      duration: "0.2",
+    });
+  } else {
     isLocked = 0;
+    container = null;
+    console.log(container);
     gsap.to(card, {
       transform: "scale(1)",
-            duration: "0.2"
+      duration: "0.2",
     });
   }
 
-  distanceFind()
+  distanceFind();
 }
 
 function mouseUp(e) {
-  let totalDistance = distanceFind()
+  let totalDistance = distanceFind();
 
   if (isLocked === 1) {
-    gsap.to(card, {
-      left: dropper.offsetLeft + "px",
-      top: dropper.offsetTop + "px",
-      duration: totalDistance * 0.0013,
-      ease: "power1.inOut",
-    });
-    gsap.to(card, {
-      transform: "scale(1)",
-            duration: "0.2"
-    });
+    //box1
+    if (container === 1) {
+      gsap.to(card, {
+        left: dropper1.offsetLeft + "px",
+        top: dropper1.offsetTop + "px",
+        duration: totalDistance * 0.0013,
+        ease: "power1.inOut",
+      });
+      gsap.to(card, {
+        transform: "scale(1)",
+        duration: "0.2",
+      });
+    }
+    //box2
+    if (container === 2) {
+      gsap.to(card, {
+        left: dropper2.offsetLeft + "px",
+        top: dropper2.offsetTop + "px",
+        duration: totalDistance * 0.0013,
+        ease: "power1.inOut",
+      });
+      gsap.to(card, {
+        transform: "scale(1)",
+        duration: "0.2",
+      });
+    } //box3
+    if (container === 3) {
+      gsap.to(card, {
+        left: dropper3.offsetLeft + "px",
+        top: dropper3.offsetTop + "px",
+        duration: totalDistance * 0.0013,
+        ease: "power1.inOut",
+      });
+      gsap.to(card, {
+        transform: "scale(1)",
+        duration: "0.2",
+      });
+    }
   }
 
   document.removeEventListener("mousemove", mouseMove);
@@ -91,13 +155,25 @@ function mouseUp(e) {
 
 function distanceFind(e) {
   const domRect1 = card.getBoundingClientRect();
+  let shoot;
+  let bang;
+  //box1
+  if (container === 1) {
+    shoot = dropper1.offsetLeft - domRect1.left;
+    bang = dropper1.offsetTop - domRect1.top;
+  }
+  //box2
+  if (container === 2) {
+    shoot = dropper2.offsetLeft - domRect1.left;
+    bang = dropper2.offsetTop - domRect1.top;
+  }
+  //box3
+  if (container === 3) {
+    shoot = dropper3.offsetLeft - domRect1.left;
+    bang = dropper3.offsetTop - domRect1.top;
+  }
 
-      let shoot = dropper.offsetLeft - domRect1.left;
-      let bang = dropper.offsetTop - domRect1.top;
+  console.log(bang, shoot);
 
-      console.log(bang, shoot);
-
-      return (Math.hypot(shoot, bang));
+  return Math.hypot(shoot, bang);
 }
-
-
