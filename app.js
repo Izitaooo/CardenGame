e5ed3c8625391b9214afd864db3f77dd77003fec
Kriztov <kriztovv@gmail.com>
2025-10-08 19:00:15 +0200

    more work on the online thingy

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const { createServer } = require('node:http');
+//socket.io
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
 const port = 3000
 
 app.use(express.static('public'))
@@ -8,6 +16,12 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+console.log("server has loaded")
