dd1be73e8577fa8e9f8669c2971e99b5b82c98b8
Kriztov <kriztovv@gmail.com>
2025-10-08 19:05:00 +0200

    fixed online thingy

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const { createServer } = require('node:http');
-//socket.io
-const { join } = require('node:path');
-const { Server } = require('socket.io');
+const express = require("express");
+const { createServer } = require("node:http");
+const { join } = require("node:path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server); // Create Socket.IO server
 
-const port = 3000
+const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
-
-io.on('connection', (socket) => {
-    console.log('a user connected');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
 
-console.log("server has loaded")
+// Use server.listen instead of app.listen
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+console.log("server has loaded");
