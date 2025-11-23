bf46546ac1c111334e5c7da5a20dd9ff60f32555
Izitaooo <petrujalex@gmail.com>
2025-11-23 18:28:36 +0100

    buyMenu stuyff

--- public/main.js
+++ public/main.js
@@ -58,24 +58,28 @@ const domRect6 = dropper5.getBoundingClientRect();
 const domRect7 = dropper6.getBoundingClientRect();
 
 
-const randBtn = document.getElementById("randBtn")
-const shortyBtn = document.getElementById("shortyBtn")
-const frenzyBtn = document.getElementById("frenzyBtn")
-const ghostBtn = document.getElementById("ghostBtn")
-const sheriffBtn = document.getElementById("sheriffBtn")
-const stingerBtn = document.getElementById("stingerBtn")
-const spectreBtn = document.getElementById("spectreBtn")
-const buckyBtn = document.getElementById("buckyBtn")
-const judgeBtn = document.getElementById("judgeBtn")
-const bulldogBtn = document.getElementById("bulldogBtn")
-const guardianBtn = document.getElementById("guardianBtn")
-const phantomBtn = document.getElementById("phantomBtn")
-const vandalBtn = document.getElementById("vandalBtn")
-const marshalBtn = document.getElementById("marshalBtn")
-const outlawBtn = document.getElementById("outlawBtn")
-const operatorBtn = document.getElementById("operatorBtn")
-const aresBtn = document.getElementById("aresBtn")
-const odinBtn = document.getElementById("odinBtn")
+const randBtn = document.getElementById("randBtn");
+const spawnButtons = document.querySelectorAll(".spawnButtons");
+
+const buttonMap = {
+  shortyBtn:     0,
+  frenzyBtn:     1,
+  ghostBtn:      2,
+  sheriffBtn:    3,
+  stingerBtn:    4,
+  spectreBtn:    5,
+  buckyBtn:      6,
+  judgeBtn:      7,
+  bulldogBtn:    8,
+  guardianBtn:   9,
+  phantomBtn:    10,
+  vandalBtn:     11,
+  marshalBtn:    12,
+  outlawBtn:     13,
+  operatorBtn:   14,
+  aresBtn:       15,
+  odinBtn:       16,
+};
 
 const hand = document.getElementById("hand");
 
@@ -898,6 +902,12 @@ cardSymb = [
   'url("images/guns/odin.png")'
 ];
 
+const priceList = [
+  300, 450, 500, 800, 1100, 1600, 850,
+  1850, 2050, 2250, 2900, 2900, 950,
+  2400, 4700, 1600, 3200
+];
+
 updateSpawnerButtons();
 credsText.innerHTML = creds;
 
@@ -911,122 +921,42 @@ function createCard(id, initialX, initialY, buttonId) {
 
   // TODO [yell]: // FRONT IMAGES
 
-  let rndNum = selectAbility(0, 3);
+  let rndNum = selectAbility(0, 16);
   let imgSelect;
   if (buttonId === "randBtn") {
     imgSelect = cardSymb[rndNum];
-  } else if (buttonId === "shortyBtn") {
-    imgSelect = cardSymb[0];
-  } else if (buttonId === "frenzyBtn") {
-    imgSelect = cardSymb[1];
-  }  else if (buttonId === "ghostBtn") {
-    imgSelect = cardSymb[2];
-  } else if (buttonId === "sheriffBtn") {
-    imgSelect = cardSymb[3];
-  } else if (buttonId === "stingerBtn") {
-    imgSelect = cardSymb[4];
-  } else if (buttonId === "spectreBtn") {
-    imgSelect = cardSymb[5];
-  } else if (buttonId === "buckyBtn") {
-    imgSelect = cardSymb[6];
-  } else if (buttonId === "judgeBtn") {
-    imgSelect = cardSymb[7];
-  } else if (buttonId === "bulldogBtn") {
-    imgSelect = cardSymb[8];
-  } else if (buttonId === "guardianBtn") {
-    imgSelect = cardSymb[9];
-  } else if (buttonId === "phantomBtn") {
-    imgSelect = cardSymb[10];
-  } else if (buttonId === "vandalBtn") {
-    imgSelect = cardSymb[11];
-  } else if (buttonId === "marshalBtn") {
-    imgSelect = cardSymb[12];
-  } else if (buttonId === "outlawBtn") {
-    imgSelect = cardSymb[13];
-  } else if (buttonId === "operatorBtn") {
-    imgSelect = cardSymb[14];
-  } else if (buttonId === "aresBtn") {
-    imgSelect = cardSymb[15];
-  } else if (buttonId === "odinBtn") {
-    imgSelect = cardSymb[16];
+  } else {
+    imgSelect = cardSymb[ buttonMap[buttonId] ];
   }
 
   cardElement.style.backgroundImage = imgSelect;
 
   // TODO [yell]: // PRICES
 
-  if(imgSelect === cardSymb[0] && buttonId !== "randBtn"){
-    cardElement.price = 300
-  }
-  else if(imgSelect === cardSymb[1] && buttonId !== "randBtn"){
-    cardElement.price = 450
-  }
-  else if(imgSelect === cardSymb[2] && buttonId !== "randBtn"){
-    cardElement.price = 500
-  }
-  else if(imgSelect === cardSymb[3] && buttonId !== "randBtn"){
-    cardElement.price = 800
-  }
-  else if(imgSelect === cardSymb[4] && buttonId !== "randBtn"){
-    cardElement.price = 1100
-  }
-  else if(imgSelect === cardSymb[5] && buttonId !== "randBtn"){
-    cardElement.price = 1600
-  }
-  else if(imgSelect === cardSymb[6] && buttonId !== "randBtn"){
-    cardElement.price = 850
-  }
-  else if(imgSelect === cardSymb[7] && buttonId !== "randBtn"){
-    cardElement.price = 1850
-  }
-  else if(imgSelect === cardSymb[8] && buttonId !== "randBtn"){
-    cardElement.price = 2050
-  }
-  else if(imgSelect === cardSymb[9] && buttonId !== "randBtn"){
-    cardElement.price = 2250
-  }
-  else if(imgSelect === cardSymb[10] && buttonId !== "randBtn"){
-    cardElement.price = 2900
-  }
-  else if(imgSelect === cardSymb[11] && buttonId !== "randBtn"){
-    cardElement.price = 2900
-  }
-  else if(imgSelect === cardSymb[12] && buttonId !== "randBtn"){
-    cardElement.price = 950
-  }
-  else if(imgSelect === cardSymb[13] && buttonId !== "randBtn"){
-    cardElement.price = 2400
-  }
-  else if(imgSelect === cardSymb[14] && buttonId !== "randBtn"){
-    cardElement.price = 4700
-  }
-  else if(imgSelect === cardSymb[15] && buttonId !== "randBtn"){
-    cardElement.price = 1600
-  }
-  else if(imgSelect === cardSymb[16] && buttonId !== "randBtn"){
-    cardElement.price = 3200
-  }
-  else {
-    cardElement.price = 0
+  if (buttonId !== "randBtn") {
+    const index = cardSymb.indexOf(imgSelect);
+    cardElement.price = priceList[index];
+  } else {
+    cardElement.price = 0;
   }
 
-  /* cardElement.infoAble = false
+    /* cardElement.infoAble = false
 
-  const ib = document.createElement("div");
-  ib.className = "cardInfo"; // hidden by default
-  ib.innerHTML = `I love cock`;
-  cardElement.appendChild(ib);
-  cardElement.infoBox = ib;
+    const ib = document.createElement("div");
+    ib.className = "cardInfo"; // hidden by default
+    ib.innerHTML = `I love cock`;
+    cardElement.appendChild(ib);
+    cardElement.infoBox = ib;
 
-  cardElement.addEventListener("click", (e) => {
-    if (handDown) return; // ignore if hand is down
-    e.stopPropagation(); // prevent bubbling if needed
-    cardElement.infoBox.classList.toggle("visible");
+    cardElement.addEventListener("click", (e) => {
+      if (handDown) return; // ignore if hand is down
+      e.stopPropagation(); // prevent bubbling if needed
+      cardElement.infoBox.classList.toggle("visible");
 
-    cardElement.infoAble = cardElement.infoAble !== true;
+      cardElement.infoAble = cardElement.infoAble !== true;
 
-    console.log("make it happen" + cardElement.infoAble);
-  }); */
+      console.log("make it happen" + cardElement.infoAble);
+    }); */
 
   const front = document.createElement("div");
   front.className = "face front";
@@ -1173,106 +1103,20 @@ function spawnCard(e) {
 
 // TODO [yell]: // PRICE KEEPERS
 
-function updateSpawnerButtons() {
-  if(creds < 300){
-    shortyBtn.style.pointerEvents = "none";
-  } else{
-    shortyBtn.style.pointerEvents = "all";
+  function updateSpawnerButtons() {
+    for (let btn of spawnButtons) {
+      const price = parseInt(btn.dataset.price) || 0;
+      if (creds >= price) {
+        // enable
+        // we should add classlist here later
+        btn.style.pointerEvents = "all";
+      } else {
+        // disable
+        // we should add classlist here later
+        btn.style.pointerEvents = "none";
+      }
+    }
   }
-
-  if(creds < 450){
-    frenzyBtn.style.pointerEvents = "none";
-  } else{
-    frenzyBtn.style.pointerEvents = "all";
-  }
-
-  if(creds < 500){
-    ghostBtn.style.pointerEvents = "none";
-  } else{
-    ghostBtn.style.pointerEvents = "all";
-  }
-
-  if(creds < 800){
-    sheriffBtn.style.pointerEvents = "none";
-  } else{
-    sheriffBtn.style.pointerEvents = "all";
-  }
-
-  if(creds < 1100){
-    stingerBtn.style.pointerEvents = "none";
-  } else{
-    stingerBtn.style.pointerEvents = "all";
-  }
-
-  if(creds < 1600){
-    spectreBtn.style.pointerEvents = "none";
-  } else{
-    spectreBtn.style.pointerEvents = "all";
-  }
-
-  if(creds < 850){
-    buckyBtn.style.pointerEvents = "none";
-  } else{
-    buckyBtn.style.pointerEvents = "all";
-  }
-
-  if(creds < 1850){
-    judgeBtn.style.pointerEvents = "none";
-  } else{
-    judgeBtn.style.pointerEvents = "all";
-  }
-
-  if(creds < 2050){
-    bulldogBtn.style.pointerEvents = "none";
-  } else{
-    bulldogBtn.style.pointerEvents = "all";
-  }
-
-  if(creds < 2250){
-    guardianBtn.style.pointerEvents = "none";
-  } else{
-    guardianBtn.style.pointerEvents = "all";
-  }
-
-  if(creds < 2900){
-    vandalBtn.style.pointerEvents = "none";
-    phantomBtn.style.pointerEvents = "none";
-  } else{
-    vandalBtn.style.pointerEvents = "all";
-    phantomBtn.style.pointerEvents = "none";
-  }
-
-  if(creds < 950){
-    marshalBtn.style.pointerEvents = "none";
-  } else{
-    marshalBtn.style.pointerEvents = "all";
-  }
-
-  if(creds < 2400){
-    outlawBtn.style.pointerEvents = "none";
-  } else{
-    outlawBtn.style.pointerEvents = "all";
-  }
-
-  if(creds < 4700){
-    operatorBtn.style.pointerEvents = "none";
-  } else{
-    operatorBtn.style.pointerEvents = "all";
-  }
-
-  if(creds < 1600){
-    aresBtn.style.pointerEvents = "none";
-  } else{
-    aresBtn.style.pointerEvents = "all";
-  }
-
-  if(creds < 3200){
-    odinBtn.style.pointerEvents = "none";
-  } else{
-    odinBtn.style.pointerEvents = "all";
-  }
-
-}
 
 let handDown = true;
 
@@ -1384,13 +1228,27 @@ function onMoveOutside(element1, element2, callback) {
 
 const menu = document.getElementById("shopMenu");
 const menuExit = document.getElementById("exit");
+let agentBtn = document.getElementById("agentBtn");
+let ab1 = document.getElementById("ab1");
+let ab2 = document.getElementById("ab2");
+let ab3 = document.getElementById("ab3");
+
 function spawnMenu() {
   menu.style.top = "4vh";
+  if(getComputedStyle(agentBtn).backgroundImage.includes("luant-s-artworks-comm-avocadocat-megu.jpg")){
+    ab1.style.backgroundImage = "url(images/Artual.jpeg)";
+  } else {
+    ab1.style.backgroundImage = "none";
+  }
 }
 
 menuExit.addEventListener("click", () => {
   menu.style.top = "100vh";
 });
+
+function changeChar(){
+
+}
 
 function updateZIndex(cardId) {
     zIndexes.splice(zIndexes.indexOf(cardId), 1);
@@ -1400,9 +1258,9 @@ function updateZIndex(cardId) {
     }
 }
 
-window.onresize = function () {
+/* window.onresize = function () {
   location.replace(location.href);
-};
+}; */
 
 function roundOver() {
   creds = creds + 200;
